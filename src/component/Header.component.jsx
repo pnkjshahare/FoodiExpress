561f4a1ff5f2bfb7126e31099666a6f65ea6af49
Pankajshahare <pankajsahare.04@gmail.com>
2025-03-23 11:31:10 +0530

    Adding Header

--- src/component/Header.component.jsx
+++ src/component/Header.component.jsx
@@ -1,7 +1,19 @@
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-blue-600">Header</h1>
+    <div className="header">
+      <div className="logo">
+        <img
+          src="https://www.shutterstock.com/image-vector/express-delivery-logo-icon-vector-260nw-1355395913.jpg"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-item">
+        <li>Location</li>
+        <li>Search</li>
+        <li>Offer</li>
+        <li>SignIn</li>
+        <li>Cart</li>
+      </div>
     </div>
   );
 };
